7dfbb17d6277b53b9b26e1bc4e8c33e3f81ca2db
ChrisMuellersCode <guitarfan@gmx.de>
2015-11-05 10:09:27 +0100

    "CreateDatabaseIfNotExists" in action

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,9 +1,27 @@
-﻿namespace De.Brickmakers.Tutorials.EF.Client
+﻿using System;
+using De.Brickmakers.Tutorials.EF.DatabaseLayer;
+using De.Brickmakers.Tutorials.EF.DatabaseLayer.Entities;
+
+namespace De.Brickmakers.Tutorials.EF.Client
 {
     class Program
     {
         static void Main(string[] args)
         {
+            using (var ctx = new SchoolContext())
+            {
+                Student stud = new Student
+                {
+                    StudentName = "Christian Müller", 
+                    DateOfBirth = new DateTime(1984,1,8)
+                };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+
+            Console.WriteLine("Done!");
+            Console.ReadKey();
         }
     }
 }
