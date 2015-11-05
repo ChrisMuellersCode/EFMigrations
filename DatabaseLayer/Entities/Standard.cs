cefc7ec7514a84b7cabe8e83b9d57aa7d92f646c
ChrisMuellersCode <guitarfan@gmx.de>
2015-11-05 09:49:03 +0100

    Created simple model and context

---
+++ DatabaseLayer/Entities/Standard.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace De.Brickmakers.Tutorials.EF.DatabaseLayer.Entities
+{
+    public class Standard
+    {
+        public Standard()
+        {
+
+        }
+        public int StandardId { get; set; }
+        public string StandardName { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+
+    }
+}
